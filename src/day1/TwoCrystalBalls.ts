a36ff4a873fe5b997b4a232e096feab874383013
Dagore <david.gomez.redondo.dev@gmail.com>
2023-07-27 12:19:20 +0200

    TwoCrystalBalls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,19 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+  const jumpAmount = Math.floor(Math.sqrt(breaks.length));
 
+  for (let i = 0; i < breaks.length; i += jumpAmount) {
+    let j;
+
+    if (breaks[i]) {
+      j = jumpAmount - i;
+      
+      while (!breaks[j]) {
+        j++;
+      }
+
+      return j;
+    }
+  }
+
+  return -1;
 }
