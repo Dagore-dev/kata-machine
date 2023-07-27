6e8c4dab465e28fe416da66664b6bbbd7f52e6f4
Dagore <david.gomez.redondo.dev@gmail.com>
2023-07-27 16:12:52 +0200

    Stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,36 @@
+type Node<T> = {
+    value: T;
+    previous?: Node<T>;
+};
+
 export default class Stack<T> {
-    public length: number;
-
-    
-
-    constructor() {
-    }
+    public length: number = 0;
+    private head?: Node<T>;
 
     push(item: T): void {
+        const node = { value: item } as Node<T>;
+        this.length++;
 
-}
+        if (!this.head) {
+            this.head = node;
+            return;
+        }
+
+        node.previous = this.head;
+        this.head = node;
+    }
     pop(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
 
-}
+        this.length--;
+        const node = this.head;
+        this.head = this.head.previous;
+
+        return node.value;
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
