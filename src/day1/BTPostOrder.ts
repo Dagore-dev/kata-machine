2978cc03108622450122c140959d30a516f807fc
Dagore <david.gomez.redondo.dev@gmail.com>
2023-07-31 08:32:28 +0200

    BinaryTree postorder traversal

--- src/day1/BTPostOrder.ts
+++ src/day1/BTPostOrder.ts
@@ -1,1 +1,17 @@
-export default function post_order_search(head: BinaryNode<number>): number[] {}
+export default function post_order_search(head: BinaryNode<number>): number[] {
+    const path: number[] = [];
+    walk(head, path);
+
+    return path;
+}
+
+function walk(current: BinaryNode<number> | null, path: number[]): void {
+    if (current == null) {
+        return;
+    }
+
+    walk(current.left, path);
+    walk(current.right, path);
+
+    path.push(current.value);
+}
