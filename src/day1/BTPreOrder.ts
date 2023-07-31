41afe37eee3602c5f6d290bfdd80244fd86f3c9a
Dagore <david.gomez.redondo.dev@gmail.com>
2023-07-31 08:21:09 +0200

    BinaryTree preorder traversal

--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,1 +1,17 @@
-export default function pre_order_search(head: BinaryNode<number>): number[] {}
+export default function pre_order_search(head: BinaryNode<number>): number[] {
+    const path: number[] = [];
+    walk(head, path);
+
+    return path;
+}
+
+function walk(current: BinaryNode<number> | null, path: number[]): void {
+    if (current == null) {
+        return;
+    }
+
+    path.push(current.value);
+
+    walk(current.left, path);
+    walk(current.right, path);
+}
