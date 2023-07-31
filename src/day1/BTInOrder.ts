a631cacdcdc9da01f908b7ac1e145070152d169f
Dagore <david.gomez.redondo.dev@gmail.com>
2023-07-31 08:28:32 +0200

    BinaryTree in order traversal

--- src/day1/BTInOrder.ts
+++ src/day1/BTInOrder.ts
@@ -1,1 +1,18 @@
-export default function in_order_search(head: BinaryNode<number>): number[] {}
+export default function in_order_search(head: BinaryNode<number>): number[] {
+    const path: number[] = [];
+    walk(head, path);
+
+    return path;
+}
+
+function walk(current: BinaryNode<number> | null, path: number[]): void {
+    if (current == null) {
+        return;
+    }
+
+    walk(current.left, path);
+
+    path.push(current.value);
+
+    walk(current.right, path);
+}
