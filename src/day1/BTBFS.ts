c8b16f0815272735a83e35c18f8fa9ff691b2186
Dagore <david.gomez.redondo.dev@gmail.com>
2023-07-31 08:54:55 +0200

    Breadth-First search

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,4 +1,23 @@
-export default function bfs(
-    head: BinaryNode<number>,
-    needle: number,
-): boolean {}
+import Queue from "./Queue";
+
+export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const q = new Queue<BinaryNode<number> | null>();
+    q.enqueue(head);
+    let current;
+
+    while (q.length) {
+        current = q.deque();
+        if (current == null) {
+            continue;
+        }
+
+        if (current?.value === needle) {
+            return true;
+        }
+
+        q.enqueue(current.left);
+        q.enqueue(current.right);
+    }
+
+    return false;
+}
